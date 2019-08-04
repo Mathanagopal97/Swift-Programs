4ce80c85595a038f472efa872d644d9581ff5c0a
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-08-04 11:27:45 +0530

    ConstantsAndVariables: Dealt with Strings and their functions

--- ConstantsAndVariables.swift
+++ ConstantsAndVariables.swift
@@ -2,7 +2,31 @@ import Foundation
 
 let notChanbable = "This cannot be changed"
 
+//Type inference
 var changable = "This can be changed"
 
 print(notChanbable)
 print(changable)
+
+//Data Types
+
+let myName = "Mathan"
+var age = 21
+let temp = 87.5
+let isRaining = true
+
+//type annotation
+let theName: String = "mathana gopal"
+let thirdFriend:String
+thirdFriend = "Venkat"
+
+
+print(theName.uppercased())
+print(theName.lowercased())
+print(theName.capitalized)
+
+print(theName)
+
+print(theName.hasPrefix("ma"))
+print(theName.hasSuffix("al"))
+print(theName.hasPrefix("Ma"))
