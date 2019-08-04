56a9a52421344df42e43cc7c3728f79575f21891
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-08-04 10:57:23 +0530

    File Upload

---
+++ ConstantsAndVariables.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+let notChanbable = "This cannot be changed"
+
+var changable = "This can be changed"
+
+print(notChanbable)
+print(changable)
--- Demo.swift
+++ Demo.swift
@@ -1,4 +1,4 @@
-//"C:\Swift\bin\swiftc.exe" -swift-version 4 "D:\Mathan\Swift Programs\<FileName>.swift" -o "C:\Swift\RuntimeEnv\Demo.exe"
+//"C:\Swift\bin\swiftc.exe" -swift-version 4 "D:\Mathan\Swift Programs\<FileName>.swift" -o "C:\Swift\RuntimeEnv\<FileName>.exe"
 //C:\Swift\RuntimeEnv\Demo.exe
 
 print("Hello World");
